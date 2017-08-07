40e1ba413e3cf38908260230cd44b8e36b696493
Ernesto Celis <developer@celisdelafuente.net>
2017-08-07 10:24:18 -0500

    UPDATE model takes timestamp to build filename later

--- fileupload/models.py
+++ fileupload/models.py
@@ -11,6 +11,7 @@ class Picture(models.Model):
     """
     file = models.FileField(upload_to="pictures")
     file_type = models.CharField(max_length=3, blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=50, blank=True)
 
     def __str__(self):
