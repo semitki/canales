9899d1e7eddc5293aae0d84c68298f5ec05cb8e7
Ernesto Celis <developer@celisdelafuente.net>
2017-08-08 13:56:42 -0500

    WIP file uploaded and start of monitoring conversion status, we'll set a recursive function wait until conversion finishes

--- fileupload/views.py
+++ fileupload/views.py
@@ -53,21 +53,62 @@ class PictureListView(ListView):
 
 class ProcessCsvView(View):
 
-    def get(self, request, *args, **kwargs):
-        sqlizer_url = 'https://sqlizer.io/api/files/'
-        sqlizer_headers = {'Authorization':
+    def __init__(self):
+        self.sqlizer_url = 'https://sqlizer.io/api/files/'
+        self.sqlizer_headers = {'Authorization':
                 'Bearer lZicTcPoMhNGbzFmd_S_xox0G2RZ5SST026wUnKstQ88TTvZWa7eFbm3j1QUkelOm-4plUPZWGkhZL7I3ZVzkQ=='}
+
+    def _monitor(self, file_id):
+        r = requests.get(self.sqlizer_url + file_id + '/',
+                headers = self.sqlizer_headers)
+        if r.status_code is 200:
+            return r.text
+        else:
+            return {'error':
+                    'Error monitoring file in sqlizer'}
+
+    def get(self, request, *args, **kwargs):
         resource_id = request.path.split('/')[2]
         csv = Picture.objects.get(pk = resource_id)
+        ts = csv.timestamp.strftime("%Y%m%d%H%M%S")
         data = {'DatabaseType': 'SQLServer',
             'FileType': 'csv',
             'FileName': csv.slug,
-            'TableName': csv.file_type,
+            'TableName': 'nw' + csv.file_type + ts,
             'FileHasHeaders': True
             }
-        r = requests.post(sqlizer_url, headers = sqlizer_headers,
+        # sqlizer step 1
+        r = requests.post(self.sqlizer_url, headers = self.sqlizer_headers,
                 data = data)
-        return JsonResponse(r.text, safe=False)
+        if r.status_code is 200:
+            response = json.loads(r.text)
+            file_id = response['ID']
+            csv_file = {'file': csv.file}
+            # sqlizer step 2
+            u = requests.post(self.sqlizer_url + file_id + '/data/',
+                    headers = self.sqlizer_headers,
+                    files = csv_file)
+            if u.status_code is 200:
+                # response = json.loads(u.text)
+                # sqlizer step 3
+                p = requests.put(self.sqlizer_url + file_id + '/',
+                        headers = self.sqlizer_headers,
+                        data = {'Status': 'Uploaded'})
+                if p.status_code is 200:
+                    response = self._monitor(file_id)
+                else:
+                    response = {'error':
+                            'Error trying to finalize file upload to sqlizer'}
+
+
+            else:
+                response = {'error':
+                        'Error trying to upload file to sqlizer'}
+        else:
+            response = {'error':
+                    'Error trying to initate file conversion on sqlizer'}
+
+        return JsonResponse(response, safe=False)
 
 
 ## maybe the shit below can go away
